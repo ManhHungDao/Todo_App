642d47b3c6bf86c4806bcb9d2ea62191739d3fbf
hung dao <hungpepi2001@gmail.com>
2022-09-12 15:29:34 +0700

    add-delete icom todo list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos, removeTodos, updateTodos } from "../redux/reducer";
 
-const Todos = () => {
+const mapStateToProps = (state) => {
+  return { todos: state };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
   return (
-    <div>Todos</div>
-  )
-}
+    <div className="addTodos">
+      <input
+        type="text"
+        className="todo.input"
+        onChange={(e) => {
+          handleChange(e);
+        }}
+      />
+      <button
+        className="add-btn"
+        onClick={() =>
+          props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+          })
+        }
+      >
+        Add
+      </button>
+      <br />
+      <ul>
+        {props.todos.map((item) => {
+          return (
+            <li key={item.id}>
+              <textarea> {item.item}</textarea>
+              <button onClick={() => props.updateTodo(item)}>Edit</button>
+              <button onClick={() => props.removeTodo(item.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default Todos
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
